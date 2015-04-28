8feb448933800858e7ad80f6375428ed1a347218
Corey Powell <mistdragon100@gmail.com>
2015-04-28 12:10:10 -0500

    Added other overloads for SFML::Window#initialize

--- include/mrb/sfml/window/window_bind.hxx
+++ include/mrb/sfml/window/window_bind.hxx
@@ -19,6 +19,15 @@ get_window(mrb_state *mrb, mrb_value self)
   return cxx_mrb_data_get_ptr<T>(mrb, self);
 }
 
+/* @!class SFML::Window
+ * @!method initialize
+ *   @overload initialize
+ *   @overload initialize(video_mode, title, style = SFML::Style::Default, context_settings = SFML::ContextSettings.new)
+ *     @param [SFML::VideoMode] video_mode
+ *     @param [String] title
+ *     @param [SFML::Style] style
+ *     @param [SFML::ContextSettings] context_settings
+ */
 template <class T>
 static mrb_value
 window_initialize(mrb_state *mrb, mrb_value self)
@@ -33,7 +42,17 @@ window_initialize(mrb_state *mrb, mrb_value self)
                                             &context_settings, &mrb_sfml_context_settings_type);
   if (argc == 0) {
     window = new sf::Window();
+  } else if (argc == 2) {
+    window = new sf::Window(*video_mode, sf::String(title));
+  } else if (argc == 3) {
+    window = new sf::Window(*video_mode, sf::String(title), style);
+  } else if (argc == 4) {
+    window = new sf::Window(*video_mode, sf::String(title), style, *context_settings);
+  } else {
+    mrb_raise(mrb, E_RUNTIME_ERROR, "Expected 0, 2, 3, or 4 arguments.");
+    return mrb_nil_value();
   }
+  cxx_mrb_data_free<sf::Window>(mrb, DATA_PTR(self));
   mrb_data_init(self, window, mrb_get_sfml_window_type<T>());
   return self;
 }
