fde6aac78571231ddef0855e558688d7ef362f93
Corey Powell <mistdragon100@gmail.com>
2015-05-05 12:36:00 -0500

    None static video_mode_class

--- src/mrb_video_mode.cxx
+++ src/mrb_video_mode.cxx
@@ -6,13 +6,12 @@
 #include "mrb/sfml/window/video_mode.hxx"
 #include "mrb_video_mode.hxx"
 
-static struct RClass *video_mode_class;
 static mrb_data_free_func video_mode_free = cxx_mrb_data_free<sf::VideoMode>;
 extern "C" const struct mrb_data_type mrb_sfml_video_mode_type = { "sf::VideoMode", video_mode_free };
 
 extern "C" void
 mrb_sfml_video_mode_init_bind(mrb_state *mrb, struct RClass *mod)
 {
-  video_mode_class = mrb_define_class_under(mrb, mod, "VideoMode", mrb->object_class);
+  struct RClass *video_mode_class = mrb_define_class_under(mrb, mod, "VideoMode", mrb->object_class);
   MRB_SET_INSTANCE_TT(video_mode_class, MRB_TT_DATA);
 }
